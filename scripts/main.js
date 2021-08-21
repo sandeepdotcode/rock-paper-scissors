4e0c048ea1d77820262907f9ca8fda109aaa45d9
Sandeep K K <sandeepdotcode@gmail.com>
2021-08-21 10:24:12 +0530

    Add function to play a round

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,19 @@ function computerPlay() {
     return play[random];
 }
 
-let output = computerPlay();
-console.log(output);
+// function to play one round of the game
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+  
+  const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
